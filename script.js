92f4113088cc434769b199edf6c2c36ef89c76cd
Richard Cole Tumolac <rctumolac@gmail.com>
2017-02-27 23:45:54 +0000

    Started to make the randomizer

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
 
+$("#studentDisplay").click(function(){
+    var Code2College_Students = ["Richard", "Tim", "Adrian", "Jorge", "Thomas", "Basil", "Abi", "Isaac", "Spencer", "Evila", "Kaci", "Rodrigo", "Jerardo", "Elizabeth", "Pamela", "Alexis"];
+    $("#rStudent").html(Code2College_Students);
+    
+});
+
+$("#teacherDisplay").click(function(){
+    var Code2College_Educators = ["Trey", "Samuel", "Sam", "Matt"];
+    $("#rTeacher").html(Code2College_Educators);
+    
+});
 
 
 });
