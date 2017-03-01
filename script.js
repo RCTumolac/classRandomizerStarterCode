532a70d1f1a2e1d06a4b6343bf3421fb452d0b4a
Richard Cole Tumolac <rctumolac@gmail.com>
2017-03-01 23:36:20 +0000

    Finished the project

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
+/* global $ */
+/* global Code2College_Students */
+/* global Code2College_Educators */
+
 $(document).ready(function(){
 
-$("#studentDisplay").click(function(){
-    var Code2College_Students = ["Richard", "Tim", "Adrian", "Jorge", "Thomas", "Basil", "Abi", "Isaac", "Spencer", "Evila", "Kaci", "Rodrigo", "Jerardo", "Elizabeth", "Pamela", "Alexis"];
-    $("#rStudent").html(Code2College_Students);
+ var Code2College_Students = ["Richard", "Tim", "Adrian", "Jorge", "Thomas", "Basil", "Abi", "Isaac", "Spencer", "Evila", "Kaci", "Rodrigo", "Jerardo", "Elizabeth", "Pamela", "Alexis"];
+
+$("#studentButton").click(function(){
+    var num = Math.floor(Math.random()*Code2College_Students.length)
+    $("#studentDisplay").html(Code2College_Students[num]);
+    
+    //console.log(Code2College_Students[num])
     
 });
 
-$("#teacherDisplay").click(function(){
+$("#teacherButton").click(function(){
     var Code2College_Educators = ["Trey", "Samuel", "Sam", "Matt"];
-    $("#rTeacher").html(Code2College_Educators);
+    $("#teacherDisplay").html(Code2College_Educators[Math.floor(Math.random()*Code2College_Educators.length)]);
+    
+    //console.log(Math.floor(Math.random()*Code2College_Educators.length))
+    
+});
+
+$("#addButton").click(function(){
+    var name = $("#name").val();
+    if(name.length === 0){
+        console.log("Nothing")
+    }
+    else{
+        Code2College_Students[Code2College_Students.length] = name;
+    }
+    
+    console.log(Code2College_Students[Code2College_Students.length-1]);
     
 });
 
